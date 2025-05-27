b55eef6c581028d3aff21cafe0251371454956c8
MX-GΔMΞCØDΞR <150932410+themxgamecoder@users.noreply.github.com>
2025-05-27 19:09:11 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,102 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const defaultOwner = '2349021506036';
+
+
+// Check for the OWNERS environment variable; if not found, use the default
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '2348165194760';  // put your number here
+
+const ownerlist = ownervb.split(';');
+
+global.owner = [];
+for (let i = 0; i < ownerlist.length; i++) {
+    global.owner.push([ownerlist[i], true]);
+}
+//
+global.botname = process.env.BOTNAME || 'MΞKΛ 𝖆𝖎';
+global.pairingNumber = process.env.BOT_NUMBER || '';  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'session id';  // put your session id here
+
+global.mods = []
+global.prems = []
+global.allowed = ['2349021506036', '2349021506036']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = [
+  '29d4b59a4aa687ca',
+  '5LTV57azwaid7dXfz5fzJu',
+  'cb15ed422c71a2fb',
+  '5bd33b276d41d6b4',
+  'HIRO',
+  'kurrxd09',
+  'ebb6251cc00f9c63',
+]
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['GataDios']
+
+global.canal = 'https://t.me/mxgamecoderr'
+
+
+global.APIs = {
+  // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz',
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api.fgmods.xyz',
+}
+global.APIKeys = {
+  // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': `${keysneoxr}`,
+  'https://violetics.pw': 'beta',
+  'https://zenzapis.xyz': `${keysxxx}`,
+  'https://api.fgmods.xyz': 'm2XBbNvz',
+}
+
+// Sticker WM
+global.premium = 'true'
+global.packname = 'MΞKΛ 𝖆𝖎'
+global.author = 'MX-GΔMΞCØDΞR'
+global.menuvid = 'https://i.ibb.co/tw4hKc9q/mx.png'
+global.igfg = '🤓 Follow on Instagram\nhttps://www.instagram.com/mxgamecoderr'
+global.dygp = 'https://t.me/mxgamecoderr'
+global.fgsc = 'https://t.me/mxgamecoderr'
+global.fgyt = 'https://youtube.com/@mxgamecoder'
+global.fgpyp = 'https://youtube.com/@mxgamecoder'
+global.fglog = 'https://i.ibb.co/tw4hKc9q/mx.png'
+global.thumb = fs.readFileSync('./meka-media/mx.jpg')
+
+global.wait = '*🕑 _processing..._*\n*▰▰▰▱▱▱▱▱*'
+global.rwait = '⏳'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌'
+global.xmoji = '🤩'
+
+global.multiplier = 69
+global.maxwarn = '3'
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
